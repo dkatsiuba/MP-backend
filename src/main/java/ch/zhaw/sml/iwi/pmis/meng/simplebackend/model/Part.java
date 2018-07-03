750b216eb060fb46d6ad3fc11ea0e96f28387361
MeiGui1 <alice_truong95@hotmail.com>
2018-07-03 11:02:14 +0200

    getter and setter

--- src/main/java/ch/zhaw/sml/iwi/pmis/meng/simplebackend/model/Part.java
+++ src/main/java/ch/zhaw/sml/iwi/pmis/meng/simplebackend/model/Part.java
@@ -7,4 +7,23 @@ public class Part {
     private String name;
     private Quantity quantity;
     private EPartType type = EPartType.STANDARD;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setQuantity(Quantity quantity) {
+        this.quantity = quantity;
+    }
+    public Quantity getQuantity() {
+        return quantity;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setType(EPartType type) {
+        this.type = type;
+    }
+    public EPartType getType() {
+        return type;
+    }
 }
